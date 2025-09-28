5487f4bfb3c6f148fbb2c6169e8a32a8419cca2e
Amr Khaled <amrkh3bkarino10@gmail.com>
2025-09-28 23:40:41 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,10 +1,11 @@
 
 def display_menu():
-    print("\nShopping List Manager")
-    print("1. Add Item")
-    print("2. Remove Item")
-    print("3. View List")
+    print("Shopping List Manager")
+    print("1. Add item")
+    print("2. Remove item")
+    print("3. View list")
     print("4. Exit")
+
 
 def main():
     shopping_list = []
